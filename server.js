a4adceb1cda8cf08d9aa3d3c040291a43cec8918
Edwin Babu <edwin.babu@cognizant.com>
2019-01-07 16:57:06 +0530

    server file added

---
+++ server.js
@@ -0,0 +1,36 @@
+const Hapi = require("hapi");
+
+// Create a server with a host and port
+const server = Hapi.server({
+  host: "localhost",
+  port: 8000
+});
+
+// Add the route
+server.route({
+  method: "GET",
+  path: "/",
+  handler: function(request, h) {
+    return "hello world";
+  },
+  config: {
+    state: {
+      parse: true,
+      failAction: "log"
+    }
+  }
+});
+
+// Start the server
+const start = async function() {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log("Server running at:", server.info.uri);
+};
+
+start();
