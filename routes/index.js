d228e8d450d7f410a550af3f528a89bf5b783801
Edwin Babu <edwin.babu@cognizant.com>
2019-01-11 17:48:26 +0530

    joi validation

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,35 @@
 const uuid = require("uuid");
 const fs = require("fs");
+const Joi = require("joi");
+
 var cards = loadCard();
+
 var configuration = {
   state: {
     parse: true,
     failAction: "log"
   }
 };
+
+var cardSchema = Joi.object().keys({
+  name: Joi.string()
+    .min(3)
+    .max(30)
+    .required(),
+  recipient_email: Joi.string()
+    .email()
+    .required(),
+  sender_name: Joi.string()
+    .min(3)
+    .max(30)
+    .required(),
+  sender_email: Joi.string()
+    .email()
+    .required(),
+  card_image: Joi.string()
+    .regex(/.+\.(jpg|bmp|png|gif)\b/)
+    .required()
+});
 
 module.exports = [
   //index page
@@ -31,6 +54,7 @@ module.exports = [
     method: "GET",
     path: "/cards",
     handler: function(request, h) {
+      console.log("test");
       return h.view("cards", { cards: cards });
     },
     config: configuration
@@ -50,16 +74,23 @@ function newCardHandler(request, h) {
     return h.view("new", { card_images: mapImages() });
   } else {
     console.log("inside post");
-    var card = {
-      name: request.payload.name,
-      recipient_email: request.payload.recipient_email,
-      sender_name: request.payload.sender_name,
-      sender_email: request.payload.sender_email,
-      card_image: request.payload.card_image
-    };
-    saveCard(card);
-    console.log(cards);
-    return h.redirect("/cards");
+    console.log("payload:", request.payload);
+    const {err, value} = Joi.validate(request.payload, cardSchema); //(err, val) => {
+      if (err) {
+        return h.response("Validation error");
+      } else {
+        let card = {
+          name: request.payload.name,
+          recipient_email: request.payload.recipient_email,
+          sender_name: request.payload.sender_name,
+          sender_email: request.payload.sender_email,
+          card_image: request.payload.card_image
+        };
+        saveCard(card);
+        console.log(cards);
+        return h.redirect("/cards");
+      }
+    // });
   }
 }
 
@@ -72,7 +103,7 @@ function saveCard(card) {
 function deleteCardHandler(request, h) {
   console.log("inside delete handler");
   delete cards[request.params.id];
-  var response = h.response("Successfully Deleted item");
+  let response = h.response("Successfully Deleted item");
   return response;
 }
 
