07d03215a29c575f6351e08e7894f3c435f271c0
edwinbbu <edwinbbu@gmail.com>
2019-01-19 14:12:32 +0530

    auth routes added

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,6 @@
+const { userStore } = require("./userStore");
+userStore.initialize();
+
 var configuration = {
   auth: false,
   state: {
@@ -14,5 +17,50 @@ module.exports = [
       return h.file("./templates/login.html");
     },
     config: configuration
+  },
+  {
+    method: "POST",
+    path: "/login",
+    handler: loginHandler,
+    config: configuration
+  },
+  {
+    method: "GET",
+    path: "/register",
+    handler: function(request, h) {
+      return h.file("./templates/register.html");
+    },
+    config: configuration
+  },
+  {
+    method: "POST",
+    path: "/register",
+    handler: registerHandler,
+    config: configuration
+  },
+  {
+    path: "/logout",
+    method: "GET",
+    handler: function(request, h) {
+      request.cookieAuth.clear();
+      return h.redirect("/");
+    },
+    config: configuration
   }
 ];
+
+function loginHandler(request, h) {
+  console.log(request.payload);
+  userStore.validateUser(
+    request.payload.email,
+    request.payload.password,
+    (err, user) => {
+      if (user) {
+        console.log("inside:", user);
+        request.cookieAuth.set(user);
+        return h.redirect("/cards");
+      }
+    }
+  );
+}
+function registerHandler(request, h) {}
--- routes/userStore.js
+++ routes/userStore.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+const Boom = require("boom");
 var userStore = {};
 
 userStore.users = {};
@@ -5,3 +7,38 @@ userStore.users = {};
 userStore.initialize = function() {
   userStore.createUser("Edwin", "edwinbbu@gmail.com", "password");
 };
+
+userStore.createUser = (name, email, password, callback) => {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+      let user = {
+        name: name,
+        email: email,
+        passwordHash: hash
+      };
+      if (userStore.users[email]) {
+        callback(Boom.conflict("Email already exists.Please log in"));
+      } else {
+        userStore.users[email] = user;
+        if (callback) callback();
+      }
+    });
+  });
+};
+
+userStore.validateUser = (email, password, callback) => {
+  let user = userStore.users[email];
+  console.log("user:", user);
+  if (!user) {
+    return callback(Boom.notFound("User does not exist."));
+  }
+  bcrypt.compare(password, user.passwordHash, (err, isValid) => {
+    if (!isValid) {
+      callback(Boom.unauthorized("Password does not match"));
+    } else {
+      callback(null, user);
+    }
+  });
+};
+
+module.exports = { userStore };
